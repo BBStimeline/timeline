3933aad92ebdefd7e40907f4b9ae438851ab86c8
曹慎凯 <caoshenkai@neotel.com.cn>
2018-04-20 14:12:45 +0800

    更换pageSwitcher

--- frontend/src/main/scala/com/neo/sk/timeline/front/pages/MainPage.scala
+++ frontend/src/main/scala/com/neo/sk/timeline/front/pages/MainPage.scala
@@ -8,19 +8,24 @@ import mhtml._
 import org.scalajs.dom
 import org.scalajs.dom._
 import org.scalajs.dom.html.TextArea
+
+import scala.xml.Elem
 /**
   * User: sky
   * Date: 2018/3/26
   * Time: 15:56
   */
 object MainPage extends Index {
+  override val locationHashString="#/LoginPage"
+
   val listGroup=Var(
     <div class="list-group">
       <a href={PageRoute.timelinePage} class="list-group-item list-group-item-success">金币管理页面</a>
       <a href={PageRoute.recordsPage} class="list-group-item list-group-item-info">记录查询页面</a>
     </div>
   )
-  def app:xml.Node = {
+
+  override def render:Elem = {
 //    CommonCheck.checkSession
     <div class="jumbotron">
       <h1>Hello, manager!</h1>
